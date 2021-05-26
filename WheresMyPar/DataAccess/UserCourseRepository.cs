9294ef79cac7eb00662d7fe65fde2e207dc17625
joe <goldglovejo13@gmail.com>
2021-05-25 20:39:16 -0500

    MVC for get request all done

--- WheresMyPar/DataAccess/UserCourseRepository.cs
+++ WheresMyPar/DataAccess/UserCourseRepository.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WheresMyPar.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace WheresMyPar.DataAccess
 {
     public class UserCourseRepository
     {
+        const string ConnectionString = "Server=localhost;Database=WheresMyPar;Trusted_Connection=True;";
+
+        //gets all users
+        public List<UserCourse> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"SELECT *
+                        FROM UserCourse";
+
+            var results = db.Query<UserCourse>(sql).ToList();
+            return results;
+        }
+
+        //gets a single user course
+        public UserCourse Get(int id)
+        {
+            var sql = @"SELECT *
+                        FROM UserCourse
+                        WHERE id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var singleUserCourse = db.QueryFirstOrDefault<UserCourse>(sql, new { id = id });
+
+            return singleUserCourse;
+        }
+
+        
+
+
     }
 }
--- WheresMyPar/DataAccess/UserCourseScoreRepository.cs
+++ WheresMyPar/DataAccess/UserCourseScoreRepository.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WheresMyPar.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace WheresMyPar.DataAccess
 {
     public class UserCourseScoreRepository
     {
+        const string ConnectionString = "Server=localhost;Database=WheresMyPar;Trusted_Connection=True;";
+
+        //gets all users course with scores
+        public List<UserCourseScore> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"SELECT *
+                        FROM UserCourseScore";
+
+            var results = db.Query<UserCourseScore>(sql).ToList();
+            return results;
+        }
+
+        //gets a single user course with scores
+        public UserCourseScore Get(int id)
+        {
+            var sql = @"SELECT *
+                        FROM UserCourseScore
+                        WHERE id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var singleUserCourseScore = db.QueryFirstOrDefault<UserCourseScore>(sql, new { id = id });
+
+            return singleUserCourseScore;
+        }
+
+
+
+
     }
 }
