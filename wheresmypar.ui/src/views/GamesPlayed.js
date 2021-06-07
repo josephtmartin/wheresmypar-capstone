96ef055bab79bfb3e0f30b5d7883c3af1e489529
joe <goldglovejo13@gmail.com>
2021-06-07 12:19:42 -0500

    got the users scores in state. now to display them

--- wheresmypar.ui/src/views/GamesPlayed.js
+++ wheresmypar.ui/src/views/GamesPlayed.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import userCourseScoresData from '../helpers/data/userCourseScoresData';
 
 export default class GamesPlayed extends React.Component {
+  state = {
+    courses: [],
+  }
+
+  componentDidMount() {
+    const { dbUser } = this.props;
+    this.getUserScores(dbUser.id);
+  }
+
+  getUserScores = (userId) => {
+    userCourseScoresData.getAllScoresForAUser(userId).then((response) => {
+      this.setState({
+        courses: response,
+      });
+    });
+  }
+
   render() {
     return (
       <>
